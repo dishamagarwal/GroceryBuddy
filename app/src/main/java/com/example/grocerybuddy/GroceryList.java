0d356c21beb06cf489c7e56c8ea292171149483b
Disha Agarwal <68860743+dishamagarwal@users.noreply.github.com>
2022-11-15 11:28:37 -0500

    clickability on navigation icons

--- app/src/main/java/com/example/grocerybuddy/GroceryList.java
+++ app/src/main/java/com/example/grocerybuddy/GroceryList.java
@@ -1,59 +1,68 @@
 package com.example.grocerybuddy;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 // declare layouts here
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link GroceryList#newInstance} factory method to
+ * Use the {@link GroceryList #newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GroceryList extends Fragment {
+public class GroceryList extends Fragment implements View.OnClickListener{
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private ArrayList<String> groceryList;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private GroceryList.Listener listener;
 
     public GroceryList() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GroceryList.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GroceryList newInstance(String param1, String param2) {
-        GroceryList fragment = new GroceryList();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+//    public InClass08FragChat(String token) {
+//        this.currentToken = token;
+//        // Required empty public constructor
+//    }
+
+    public interface Listener {
+        void goToFragGroceryListView();
+        void goToFragScanView();
+        void goToFragAddItemView();
+        void goToSettings();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof GroceryList.Listener) {
+            listener = (GroceryList.Listener) context;
+        } else {
+            throw new RuntimeException(context.toString() + "must implement Listener");
+        }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        // TODO: fetch any existing note for this user and init it
+        groceryList = new ArrayList<>();
+        groceryList.add("Example item here!");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getActivity().setTitle("Grocery List");
     }
 
     @Override
@@ -63,5 +72,24 @@ public class GroceryList extends Fragment {
         // Inflate the layout for this fragment
         //initialize layouts here
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.listView_imageButton:
+                break;
+            case R.id.addItemView_imageButton:
+                listener.goToFragAddItemView();
+                break;
+            case R.id.scanView_imageButton:
+                listener.goToFragScanView();
+                break;
+            case R.id.settings:
+                listener.goToSettings();
+                break;
+            default:
+                throw new IllegalStateException("Invalid View ID - This should be unreachable");
+        }
     }
 }
